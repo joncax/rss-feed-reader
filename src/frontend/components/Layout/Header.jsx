a87f28d588c08f3b4d2305531ac956c27e92e136
joncax <jonlmcardoso@gmail.com>
2026-03-24 18:40:16 +0000

    Chore: Remove PropTypes from UI components (Header.jsx)

--- src/frontend/components/Layout/Header.jsx
+++ src/frontend/components/Layout/Header.jsx
@@ -5,7 +5,6 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 
 export default function Header() {
   const [lastUpdate, setLastUpdate] = useState(null);
